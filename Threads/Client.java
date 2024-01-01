8f17c3ce655ce35b5f0618b40fc93d6c865abc32
ARPIT _VAJRANGI <arpitvajrangi99@gmail.com>
2024-01-01 21:05:19 +0530

    Task to run Thread 100 times with comments

--- Threads/Client.java
+++ Threads/Client.java
@@ -5,10 +5,12 @@ public class Client {
         System.out.println("Hello World from main..."+Thread.currentThread().getName());
 
 
-
+    //Task: Run a print statement 100 times where each task should be done by a new thread.
          for(int i=0;i<=100;i++){
-             HelloWorld hwc=new HelloWorld();
-             Thread t=new Thread(hwc);
+             HelloWorld hwc=new HelloWorld();//creating the object of the task(class)
+             //which we need to run in a different thread
+             //putting it under for loop so that it can create a new thread everytime
+             Thread t=new Thread(hwc);//Passing a task to thread which it will have to run
              t.start();
          }
     }
